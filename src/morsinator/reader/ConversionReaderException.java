29aa904f8ab311752c182d9ef933ed2d4c814d13
Nathan HANEN <hanen.nathan@gmail.com>
2021-09-25 17:48:35 +0200

    more custom exception constructor options

--- src/morsinator/reader/ConversionReaderException.java
+++ src/morsinator/reader/ConversionReaderException.java
@@ -3,8 +3,18 @@ package morsinator.reader;
 public class ConversionReaderException extends RuntimeException {
     private int row;
 
+    public ConversionReaderException(int row) {
+        super();
+        this.row = row;
+    }
+
     public ConversionReaderException(String message, int row) {
         super(message);
+        this.row = row;
+    }
+
+    public ConversionReaderException(int row, Throwable cause) {
+        super(cause);
         this.row = row;
     }
 
