442d1dd62c8a25beda4cd08105626cd13ad4eb2a
sibzou <91130460+sibzou@users.noreply.github.com>
2021-09-26 14:55:06 +0200

    main: Parse the conversion table

--- src/morsinator/Morsinator.java
+++ src/morsinator/Morsinator.java
@@ -1,5 +1,11 @@
 package morsinator;
 
+import morsinator.reader.ConversionRow;
+import morsinator.reader.ConversionReader;
+import morsinator.reader.BinaryConversionReader;
+import morsinator.collections.MorsiList;
+import morsinator.reader.ConversionReaderException;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
@@ -19,6 +25,17 @@ public class Morsinator {
             conversionFile = new FileInputStream(args[1]);
         } catch(FileNotFoundException exception) {
             System.err.println("Table de conversion introuvable");
+            System.exit(1);
+        }
+
+        ConversionReader conversionReader = new BinaryConversionReader();
+        MorsiList<ConversionRow> morsiList = new MorsiList<ConversionRow>();
+
+        try {
+            conversionReader.fill(conversionFile, morsiList, null);
+        } catch(ConversionReaderException exception) {
+            System.err.println("Erreur de lecture de la table de conversion\n" +
+                args[1] + ":" + exception.getRow() + " : " + exception.getMessage());
             System.exit(1);
         }
     }
