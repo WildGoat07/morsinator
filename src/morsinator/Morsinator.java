8b55f35bc21c53318129741fc94773e38d85ccf8
sibzou <91130460+sibzou@users.noreply.github.com>
2021-09-26 14:54:08 +0200

    main: Open the conversion table

--- src/morsinator/Morsinator.java
+++ src/morsinator/Morsinator.java
@@ -1,5 +1,8 @@
 package morsinator;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Morsinator {
     public static void main(String[] args) {
         if(args.length != 4) {
@@ -7,6 +10,15 @@ public class Morsinator {
                                "Options :\n" +
                                "    -tm  --texte-morse    Convertit de texte vers morse\n" +
                                "    -mt  --morse-texte    Convertit de morse vers texte");
+            System.exit(1);
+        }
+
+        FileInputStream conversionFile = null;
+
+        try {
+            conversionFile = new FileInputStream(args[1]);
+        } catch(FileNotFoundException exception) {
+            System.err.println("Table de conversion introuvable");
             System.exit(1);
         }
     }
