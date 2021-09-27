f224e97d5d2e2b3dbf662f64b3c597c1dff33e41
sibzou <91130460+sibzou@users.noreply.github.com>
2021-09-27 15:37:56 +0200

    Merge the logic of BinaryConversionReader in TextualConversionReader

--- src/morsinator/reader/TextualConversionReader.java
+++ src/morsinator/reader/TextualConversionReader.java
@@ -8,60 +8,72 @@ import morsinator.collections.MorsiList;
 import morsinator.reader.ConversionReaderException;
 
 public class TextualConversionReader implements ConversionReader {
+    private int readReader(Reader reader, int row) throws ConversionReaderException {
+        try {
+            return reader.read();
+        } catch(IOException e) {
+            throw new ConversionReaderException("Erreur de lecture du fichier", row, e);
+        }
+    }
 
     @Override
     public void fill(Reader reader, MorsiList<ConversionRow> tm, MorsiBinaryTree<String, Character> mt) throws ConversionReaderException {
         int row = 1;
         HashSet<String> addedLetters = new HashSet<>();
         tm.clear();
-        try {
-            while (true) {
-                String key = "";
-                String value = "";
-                int currentChar;
+
+        String key = "";
+        String value = null;
+        boolean readingKey = true; // if false, reading value
+        int currentChar = readReader(reader, row);
+
+        while(currentChar != -1) {
+            if(currentChar == '\n')
+                ++row;
+
+            if(readingKey) {
                 // étape 1 : lecture de la clé (la lettre)
-                while ((currentChar = reader.read()) != -1 && currentChar != '=') {
-                    if (currentChar == '\n')
-                        ++row;
+                if(currentChar == '=') {
+                    key = key.trim().toUpperCase();
+
+                    // vérifications d'intégrités
+                    if(key.length() != 1)
+                        throw new ConversionReaderException("Lettre invalide", row);
+                    else if(addedLetters.contains(key))
+                        throw new ConversionReaderException("Lettre déjà ajoutée", row);
+
+                    addedLetters.add(key);
+                    readingKey = false;
+                    value = "";
+                } else {
                     key += (char) currentChar;
                 }
-                key = key.trim().toUpperCase();
-                // si la clé est vide et qu'on est à la fin du fichier, on arrête
-                if (currentChar == -1 && key.isEmpty())
-                    break;
-                // vérifications d'intégrités
-                if (currentChar == -1)
-                    throw new ConversionReaderException("Fin de fichier inattendue", row);
-                if (key.length() != 1)
-                    throw new ConversionReaderException("Lettre invalide", row);
-                if (addedLetters.contains(key))
-                    throw new ConversionReaderException("Lettre déjà ajoutée", row);
-                else
-                    addedLetters.add(key);
+            } else {
                 // étape 2 : lecture de la valeur (le code morse)
-                while ((currentChar = reader.read()) != -1 && (currentChar != '\n' || value.trim().isEmpty())) {
-                    if (currentChar == '\n')
-                        ++row;
+                if(currentChar == '\n' && !value.trim().isEmpty()) {
+                    value = value.trim();
+                    tm.add(new ConversionRow(key.charAt(0), value));
+
+                    if(mt.get(value) != null)
+                        throw new ConversionReaderException("Code morse déjà ajouté sous la lettre '" + mt.get(value) + "'", row);
+
+                    try {
+                        mt.set(value, key.charAt(0));
+                    } catch(RuntimeException e) {
+                        throw new ConversionReaderException(row, e);
+                    }
+
+                    readingKey = true;
+                    key = "";
+                } else {
                     value += (char) currentChar;
                 }
-                ++row;
-                value = value.trim();
-                tm.add(new ConversionRow(key.charAt(0), value));
-
-                if (mt.get(value) != null)
-                    throw new ConversionReaderException("Code morse déjà ajouté sous la lettre '" + mt.get(value), row);
-                try {
-                    mt.set(value, key.charAt(0));
-                } catch (RuntimeException e) {
-                    throw new ConversionReaderException(row, e);
-                }
-                // si on est à la fin du fichier, on arrête
-                if (currentChar == -1)
-                    break;
             }
-        } catch (IOException e) {
-            throw new ConversionReaderException("Erreur de lecture du fichier", row, e);
-        }
-    }
 
+            currentChar = readReader(reader, row);
+        }
+
+        if(readingKey && !key.trim().isEmpty())
+            throw new ConversionReaderException("Fin de fichier inattendue", row);
+    }
 }
