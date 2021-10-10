2a5c635fb806438acc42a5598741110c4fd8f9fc
sibzou <91130460+sibzou@users.noreply.github.com>
2021-10-10 18:31:08 +0200

    Add the column support to TextPosition

--- src/morsinator/text/TextPosition.java
+++ src/morsinator/text/TextPosition.java
@@ -2,16 +2,27 @@ package morsinator.text;
 
 public class TextPosition {
     private int row;
+    private int col;
 
     public TextPosition() {
         row = 1;
+        col = 1;
+    }
+
+    protected void newChar() {
+        col++;
     }
 
     protected void lineFeed() {
         row++;
+        col = 1;
     }
 
     public int getRow() {
         return row;
+    }
+
+    public int getCol() {
+        return col;
     }
 }
