a50f6c4049c33a498eb104bdb0113c0a4f0786c4
Nathan HANEN <hanen.nathan@gmail.com>
2021-10-11 13:43:55 +0200

    fix: :rotating_light: compile with UTF8 encoding

--- compile.cmd
+++ compile.cmd
@@ -1,3 +1,3 @@
-d:/ProgramFiles/jdk-17/bin/javac.exe --module-path d:/ProgramFiles/javafx-sdk-17.0.0.1/lib --add-modules javafx.controls,javafx.fxml -d bin @sources.txt
+e:/ProgramFiles/jdk-17/bin/javac.exe -encoding UTF8 --module-path e:/ProgramFiles/javafx-sdk-17.0.0.1/lib --add-modules javafx.controls,javafx.fxml -d bin @sources.txt
 cp ./src/morsinator/interfaces/MainWindow.fxml ./bin/morsinator/interfaces/MainWindow.fxml
 cp -r assets bin
