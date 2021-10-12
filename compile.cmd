ed9da2f25ea1277f23c8142fb54f5d2fb2e640b9
Nathan Hanen <hanen4u@etu.univ-lorraine.fr>
2021-10-12 10:36:07 +0200

    build: script copies `EditTable.fxml`

--- compile.cmd
+++ compile.cmd
@@ -1,3 +1,4 @@
 e:/ProgramFiles/jdk-17/bin/javac.exe -encoding UTF8 --module-path e:/ProgramFiles/javafx-sdk-17.0.0.1/lib --add-modules javafx.controls,javafx.fxml -d bin @sources.txt
 cp ./src/morsinator/interfaces/MainWindow.fxml ./bin/morsinator/interfaces/MainWindow.fxml
+cp ./src/morsinator/interfaces/EditTable.fxml ./bin/morsinator/interfaces/EditTable.fxml
 cp -r assets bin
